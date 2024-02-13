0616e250632d5a908a358c4d7e13be6427b08738
nehapatel654 <158989899+nehapatel654@users.noreply.github.com>
2024-02-13 14:17:47 +0530

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,41 @@
+# # import numpy as np
+# # a=np.array([120,12,1])
+# # print(a)
+
+# # 1-d arrys
+
+# # import numpy as np
+# # a=np.array([1,2,3,4])
+# # print(a)
+
+
+# # //2d arrays
+# import numpy as np
+# a=np.array([[1,2,3],[4,5,6]])
+# print(a)
+
+# import numpy as np
+# a=np.array([[[1,2,4],[5,6,8],[1,3,5]]])
+# print(a)
+
+# import numpy as np
+# a=np.array([[[2,3,4],[5,6,7],[8,9,10]]])
+# print(a)
+
+# import numpy as a1
+# # a=a1.array([[[2,3,4,5,6,7,8,9,10]]])
+# # b=a.reshape(3,3)
+# # print(b)  
+
+# list=a1.arange(2,11)
+# a=a1.array([list])
+# c=a.reshape(3,3)      
+# print(c)  
+    
+    
+import numpy as a1
+a=a1.array([1,2,3,4,5,6])
+b=a.reshape(3,2)
+c=b.reshape(2,3)
+print(b)
+print(c)
